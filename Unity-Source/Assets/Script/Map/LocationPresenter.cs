746408119f2398c5bc6a02581854f4695727155d
ogasawarasatoru <world.wide.web.13@gmail.com>
2021-03-08 23:00:51 +0900

    3D-CharacterAnimation

--- Unity-Source/Assets/Script/Map/LocationPresenter.cs
+++ Unity-Source/Assets/Script/Map/LocationPresenter.cs
@@ -1,16 +1,26 @@
 ﻿using System.Collections;
 using UnityEngine;
 using UnityEngine.Networking;
+using UnityEngine.UI;
 
 public class LocationPresenter : MonoBehaviour
 {
+        // imageオブジェクトの取得
+        GameObject MapImage;
+        // imageコンポーネントの取得
+        Image MapImage_Component;
+    
     // GameObjectをメソッド外で定義することで、InspectorにてObjectを紐づけできる。
-    private const string STATIC_MAP_URL = "https://maps.googleapis.com/maps/api/staticmap?key=AIzaSyCN773NQ0NQsV4OjQjSd-TuFv-5Z9IEbWU&zoom=15&size=640x640&scale=2&maptype=terrain&style=element:labels|visibility:off";
+    private const string STATIC_MAP_URL = "https://maps.googleapis.com/maps/api/staticmap?key=AIzaSyCN773NQ0NQsV4OjQjSd-TuFv-5Z9IEbWU&zoom=10&size=240x240&scale=2&maptype=terrain&style=element:labels|visibility:off";
 
     private int frame = 0;
 
     void Start()
     {
+        // imageオブジェクトの取得
+            MapImage = GameObject.Find("MapImage");
+        // imageコンポーネントの取得
+            MapImage_Component = MapImage.GetComponent<Image>();
         // 非同期処理
         StartCoroutine(getStaticMap());       
     }
@@ -39,30 +49,51 @@ public class LocationPresenter : MonoBehaviour
             // string.Format("{}" hogehoge )...{}番目の値を"{}"の形式で表示する ※{},引数は複数指定可
             // 例:string.Format("{0}です" hogehoge, hogeeeeee ) = hogehogeです
             query += "&center=" + UnityWebRequest.UnEscapeURL(string.Format("{0},{1}", Input.location.lastData.latitude, Input.location.lastData.longitude));
-            // query += "&center=35.770160,139.645000";
-            Debug.Log(query);
         // markersで渡した座標(=現在位置)にマーカーを立てる
             query += "&markers=" + UnityWebRequest.UnEscapeURL(string.Format("{0},{1}", Input.location.lastData.latitude, Input.location.lastData.longitude));
-            // query += "&markers=35.770160,139.645000";
             Debug.Log(query);
-
+        
         //リクエストの定義
         // UnityWebRequestTexture.GetTexture(hogehoge)...HTTP GET経由でイメージをダウンロードするときに
         // hogehogeを基にTextureを作成するメソッド
             var req = UnityWebRequestTexture.GetTexture(STATIC_MAP_URL + query);
+            Debug.Log(STATIC_MAP_URL + query);
+
         // リクエスト実行
         // yield return XX; ... XXの間、処理を停止
             yield return req.SendWebRequest();
 
             if (req.error == null)
             {
+                Debug.Log("マップをspriteに書き出します");              
                 // 既に表示しているマップを更新
                 // レンダラー...オブジェクトを画面に描画するためのもの
                 // マテリアルのテクスチャ...オブジェクトに質感や見た目を変えるためのもの
                 // material.mainTexture... LocationPresenter.cs がアタッチされているマテリアルそのものを指す、と予想
-                    Destroy(GetComponent<Renderer>().material.mainTexture);
+                    // Destroy(GetComponent<Renderer>().material.mainTexture);
                 // DownloadHandlerTexture()...画像データのダウンロードに関するサブクラス
-                    GetComponent<Renderer>().material.mainTexture = ((DownloadHandlerTexture)req.downloadHandler).texture;
+                    // GetComponent<Renderer>().material.mainTexture = ((DownloadHandlerTexture)req.downloadHandler).texture;
+
+                // スプライト（インスタンス）を明示的に開放
+                    if (MapImage_Component.sprite != null)
+                    {
+                        Destroy(MapImage_Component.sprite);
+                        yield return null;
+                        MapImage_Component.sprite = null;
+                        yield return null;
+                        Debug.Log("初期化完了");                        
+                    }
+                
+                // downloadHandlerをDownloadHandlerTextureにキャストしてテクスチャを取得
+                    var texture = (req.downloadHandler as DownloadHandlerTexture).texture;
+                    Debug.Log("テクスチャを取得: "  + texture);              
+                // down
+                    MapImage_Component.sprite = Sprite.Create(
+                        texture: texture,
+                        rect : new Rect(0, 0, 480, 480),
+                        pivot : new Vector2(0.5f, 0.5f)
+                    );
+
             }
     }
 }
--- Unity-Source/Assets/Script/Map/ScrollView.cs
+++ Unity-Source/Assets/Script/Map/ScrollView.cs
@@ -7,18 +7,29 @@ public class ScrollView : MonoBehaviour
 {
     public GameObject DebugText;
     public GameObject DebugWindow;
+    public int logcnt = 0;
+
+    private Text _logText;
 
     void Awake()
     {
         Application.logMessageReceived += LoggedCb;  // ログ出力時のコールバックを登録
+        _logText = DebugText.GetComponent<Text>();
     }
 
     // Start と Updateは省略
 
     public void LoggedCb(string logstr, string stacktrace, LogType type)
     {
-        DebugText.GetComponent<Text>().text += logstr;
-        DebugText.GetComponent<Text>().text += "\n";
+        if (logcnt > 200) {
+            int index = _logText.text.IndexOf("\n");
+            _logText.text = _logText.text.Substring(index + 1);
+        } else {
+            logcnt++;
+        }
+
+        _logText.text += logstr;
+        _logText.text += "\n";
         // 常にTextの最下部（最新）を表示するように強制スクロール
         DebugWindow.GetComponent<ScrollRect>().verticalNormalizedPosition = 0;
     }
